13d08a1829d695566e9d842d76e66429a1a523c2
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-06-02 21:47:40 +0200

    Almost works

--- AutoUpdater.NET/UpdateForm.Designer.cs
+++ AutoUpdater.NET/UpdateForm.Designer.cs
@@ -2,148 +2,151 @@
 
 namespace AutoUpdaterDotNET
 {
-    sealed partial class UpdateForm
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	sealed partial class UpdateForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateForm));
-            this.webBrowser = new System.Windows.Forms.WebBrowser();
-            this.labelUpdate = new System.Windows.Forms.Label();
-            this.labelDescription = new System.Windows.Forms.Label();
-            this.labelReleaseNotes = new System.Windows.Forms.Label();
-            this.buttonUpdate = new System.Windows.Forms.Button();
-            this.buttonRemindLater = new System.Windows.Forms.Button();
-            this.pictureBoxIcon = new System.Windows.Forms.PictureBox();
-            this.buttonSkip = new System.Windows.Forms.Button();
-            this.webView2 = new Microsoft.Web.WebView2.WinForms.WebView2();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.webView2)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // webBrowser
-            // 
-            resources.ApplyResources(this.webBrowser, "webBrowser");
-            this.webBrowser.Name = "webBrowser";
-            this.webBrowser.ScriptErrorsSuppressed = true;
-            // 
-            // labelUpdate
-            // 
-            resources.ApplyResources(this.labelUpdate, "labelUpdate");
-            this.labelUpdate.Name = "labelUpdate";
-            // 
-            // labelDescription
-            // 
-            resources.ApplyResources(this.labelDescription, "labelDescription");
-            this.labelDescription.Name = "labelDescription";
-            // 
-            // labelReleaseNotes
-            // 
-            resources.ApplyResources(this.labelReleaseNotes, "labelReleaseNotes");
-            this.labelReleaseNotes.Name = "labelReleaseNotes";
-            // 
-            // buttonUpdate
-            // 
-            resources.ApplyResources(this.buttonUpdate, "buttonUpdate");
-            this.buttonUpdate.Image = global::AutoUpdaterDotNET.Properties.Resources.download;
-            this.buttonUpdate.Name = "buttonUpdate";
-            this.buttonUpdate.UseVisualStyleBackColor = true;
-            this.buttonUpdate.Click += new System.EventHandler(this.ButtonUpdateClick);
-            // 
-            // buttonRemindLater
-            // 
-            resources.ApplyResources(this.buttonRemindLater, "buttonRemindLater");
-            this.buttonRemindLater.Image = global::AutoUpdaterDotNET.Properties.Resources.clock_go;
-            this.buttonRemindLater.Name = "buttonRemindLater";
-            this.buttonRemindLater.UseVisualStyleBackColor = true;
-            this.buttonRemindLater.Click += new System.EventHandler(this.ButtonRemindLaterClick);
-            // 
-            // pictureBoxIcon
-            // 
-            this.pictureBoxIcon.Image = global::AutoUpdaterDotNET.Properties.Resources.update;
-            resources.ApplyResources(this.pictureBoxIcon, "pictureBoxIcon");
-            this.pictureBoxIcon.Name = "pictureBoxIcon";
-            this.pictureBoxIcon.TabStop = false;
-            // 
-            // buttonSkip
-            // 
-            resources.ApplyResources(this.buttonSkip, "buttonSkip");
-            this.buttonSkip.DialogResult = System.Windows.Forms.DialogResult.Abort;
-            this.buttonSkip.Image = global::AutoUpdaterDotNET.Properties.Resources.hand_point;
-            this.buttonSkip.Name = "buttonSkip";
-            this.buttonSkip.UseVisualStyleBackColor = true;
-            this.buttonSkip.Click += new System.EventHandler(this.ButtonSkipClick);
-            // 
-            // webView2
-            // 
-            this.webView2.AllowExternalDrop = true;
-            this.webView2.CreationProperties = null;
-            this.webView2.DefaultBackgroundColor = System.Drawing.Color.White;
-            resources.ApplyResources(this.webView2, "webView2");
-            this.webView2.Name = "webView2";
-            this.webView2.ZoomFactor = 1D;
-            // 
-            // UpdateForm
-            // 
-            this.AcceptButton = this.buttonUpdate;
-            resources.ApplyResources(this, "$this");
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.pictureBoxIcon);
-            this.Controls.Add(this.labelReleaseNotes);
-            this.Controls.Add(this.labelDescription);
-            this.Controls.Add(this.labelUpdate);
-            this.Controls.Add(this.buttonUpdate);
-            this.Controls.Add(this.buttonSkip);
-            this.Controls.Add(this.buttonRemindLater);
-            this.Controls.Add(this.webView2);
-            this.Controls.Add(this.webBrowser);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "UpdateForm";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UpdateForm_FormClosing);
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.UpdateForm_FormClosed);
-            this.Load += new System.EventHandler(this.UpdateFormLoad);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.webView2)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateForm));
+			webBrowser = new System.Windows.Forms.WebBrowser();
+			labelDescription = new ReaLTaiizor.Controls.PoisonLabel();
+			labelReleaseNotes = new ReaLTaiizor.Controls.PoisonLabel();
+			buttonUpdate = new ReaLTaiizor.Controls.PoisonButton();
+			buttonRemindLater = new ReaLTaiizor.Controls.PoisonButton();
+			buttonSkip = new ReaLTaiizor.Controls.PoisonButton();
+			webView2 = new Microsoft.Web.WebView2.WinForms.WebView2();
+			poisonStyleManager = new ReaLTaiizor.Manager.PoisonStyleManager(components);
+			poisonStyleExtender = new ReaLTaiizor.Controls.PoisonStyleExtender(components);
+			((System.ComponentModel.ISupportInitialize)webView2).BeginInit();
+			((System.ComponentModel.ISupportInitialize)poisonStyleManager).BeginInit();
+			SuspendLayout();
+			// 
+			// webBrowser
+			// 
+			resources.ApplyResources(webBrowser, "webBrowser");
+			poisonStyleExtender.SetApplyPoisonTheme(webBrowser, true);
+			webBrowser.Name = "webBrowser";
+			webBrowser.ScriptErrorsSuppressed = true;
+			// 
+			// labelDescription
+			// 
+			resources.ApplyResources(labelDescription, "labelDescription");
+			labelDescription.Name = "labelDescription";
+			labelDescription.WrapToLine = true;
+			// 
+			// labelReleaseNotes
+			// 
+			resources.ApplyResources(labelReleaseNotes, "labelReleaseNotes");
+			labelReleaseNotes.Name = "labelReleaseNotes";
+			labelReleaseNotes.Style = ReaLTaiizor.Enum.Poison.ColorStyle.Blue;
+			labelReleaseNotes.UseStyleColors = true;
+			// 
+			// buttonUpdate
+			// 
+			resources.ApplyResources(buttonUpdate, "buttonUpdate");
+			buttonUpdate.Highlight = true;
+			buttonUpdate.Name = "buttonUpdate";
+			buttonUpdate.Style = ReaLTaiizor.Enum.Poison.ColorStyle.Blue;
+			buttonUpdate.UseSelectable = true;
+			buttonUpdate.UseStyleColors = true;
+			buttonUpdate.Click += ButtonUpdateClick;
+			// 
+			// buttonRemindLater
+			// 
+			resources.ApplyResources(buttonRemindLater, "buttonRemindLater");
+			buttonRemindLater.Name = "buttonRemindLater";
+			buttonRemindLater.Style = ReaLTaiizor.Enum.Poison.ColorStyle.Blue;
+			buttonRemindLater.UseSelectable = true;
+			buttonRemindLater.UseStyleColors = true;
+			buttonRemindLater.Click += ButtonRemindLaterClick;
+			// 
+			// buttonSkip
+			// 
+			buttonSkip.DialogResult = System.Windows.Forms.DialogResult.Abort;
+			resources.ApplyResources(buttonSkip, "buttonSkip");
+			buttonSkip.Name = "buttonSkip";
+			buttonSkip.Style = ReaLTaiizor.Enum.Poison.ColorStyle.Blue;
+			buttonSkip.UseSelectable = true;
+			buttonSkip.UseStyleColors = true;
+			buttonSkip.UseVisualStyleBackColor = true;
+			buttonSkip.Click += ButtonSkipClick;
+			// 
+			// webView2
+			// 
+			webView2.AllowExternalDrop = true;
+			resources.ApplyResources(webView2, "webView2");
+			poisonStyleExtender.SetApplyPoisonTheme(webView2, true);
+			webView2.CreationProperties = null;
+			webView2.DefaultBackgroundColor = System.Drawing.Color.White;
+			webView2.Name = "webView2";
+			webView2.ZoomFactor = 1D;
+			// 
+			// poisonStyleManager
+			// 
+			poisonStyleManager.Owner = this;
+			// 
+			// UpdateForm
+			// 
+			AcceptButton = buttonUpdate;
+			resources.ApplyResources(this, "$this");
+			AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			BackImage = Properties.Resources.update;
+			BackImagePadding = new System.Windows.Forms.Padding(580, 25, 0, 0);
+			BackMaxSize = 50;
+			Controls.Add(labelReleaseNotes);
+			Controls.Add(labelDescription);
+			Controls.Add(buttonUpdate);
+			Controls.Add(buttonSkip);
+			Controls.Add(buttonRemindLater);
+			Controls.Add(webView2);
+			Controls.Add(webBrowser);
+			MaximizeBox = false;
+			Name = "UpdateForm";
+			ShadowType = ReaLTaiizor.Enum.Poison.FormShadowType.AeroShadow;
+			StyleManager = poisonStyleManager;
+			FormClosing += UpdateForm_FormClosing;
+			FormClosed += UpdateForm_FormClosed;
+			Load += UpdateFormLoad;
+			((System.ComponentModel.ISupportInitialize)webView2).EndInit();
+			((System.ComponentModel.ISupportInitialize)poisonStyleManager).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        }
+		#endregion
 
-        #endregion
-
-        private System.Windows.Forms.Button buttonRemindLater;
-        private System.Windows.Forms.Button buttonUpdate;
-        private System.Windows.Forms.Button buttonSkip;
-        private System.Windows.Forms.WebBrowser webBrowser;
-        private System.Windows.Forms.Label labelUpdate;
-        private System.Windows.Forms.Label labelDescription;
-        private System.Windows.Forms.Label labelReleaseNotes;
-        private System.Windows.Forms.PictureBox pictureBoxIcon;
-        private Microsoft.Web.WebView2.WinForms.WebView2 webView2;
-    }
+		private ReaLTaiizor.Controls.PoisonButton buttonRemindLater;
+		private ReaLTaiizor.Controls.PoisonButton buttonUpdate;
+		private ReaLTaiizor.Controls.PoisonButton buttonSkip;
+		private System.Windows.Forms.WebBrowser webBrowser;
+		private ReaLTaiizor.Controls.PoisonLabel labelDescription;
+		private ReaLTaiizor.Controls.PoisonLabel labelReleaseNotes;
+		private Microsoft.Web.WebView2.WinForms.WebView2 webView2;
+		private ReaLTaiizor.Manager.PoisonStyleManager poisonStyleManager;
+		private ReaLTaiizor.Controls.PoisonStyleExtender poisonStyleExtender;
+	}
 }
