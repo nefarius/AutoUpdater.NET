07ec6a8c80f1fed0603ffe7610171317f81fa473
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-06-02 19:01:55 +0200

    Revert "Changed buttons"
    This reverts commit c041f5ce169c90ad11eb75ba4c62f5efb33b5afe.
    
    Revert "Changed Update form to MaterialForm"
    
    This reverts commit 44a4e31bbdd33d3cecce4ae9cb3aac802f077655.
    
    Revert "Changed picture box background to transparent"
    
    This reverts commit d83edcd380613a490b04395ac20666342d0925af.
    
    Revert "Replaced labels"
    
    This reverts commit 20081533b4383ce01233eafc247a2704a1325936.
    
    Revert "Changed UpdateForm to MetroForm"
    
    This reverts commit cb8c3463dba93d73002d8073ef834d68b572ece3.

--- AutoUpdater.NET/UpdateForm.cs
+++ AutoUpdater.NET/UpdateForm.cs
@@ -7,254 +7,235 @@ using System.IO;
 using System.Windows.Forms;
 using Microsoft.Web.WebView2.Core;
 using Microsoft.Win32;
-using ReaLTaiizor.Colors;
-using ReaLTaiizor.Forms;
-using ReaLTaiizor.Manager;
-using ReaLTaiizor.Util;
 using static System.Reflection.Assembly;
 
 namespace AutoUpdaterDotNET;
 
-internal sealed partial class UpdateForm : MaterialForm
+internal sealed partial class UpdateForm : Form
 {
-	private readonly UpdateInfoEventArgs _args;
-	private readonly MaterialSkinManager _materialSkinManager;
+    private readonly UpdateInfoEventArgs _args;
 
-	public UpdateForm(UpdateInfoEventArgs args)
-	{
+    public UpdateForm(UpdateInfoEventArgs args)
+    {
+        _args = args;
+        InitializeComponent();
+        InitializeBrowserControl();
+        buttonSkip.Visible = AutoUpdater.ShowSkipButton;
+        buttonRemindLater.Visible = AutoUpdater.ShowRemindLaterButton;
+        var resources = new ComponentResourceManager(typeof(UpdateForm));
+        Text = string.Format(resources.GetString("$this.Text", CultureInfo.CurrentCulture)!,
+            AutoUpdater.AppTitle, _args.CurrentVersion);
+        labelUpdate.Text = string.Format(resources.GetString("labelUpdate.Text", CultureInfo.CurrentCulture)!,
+            AutoUpdater.AppTitle);
+        labelDescription.Text =
+            string.Format(resources.GetString("labelDescription.Text", CultureInfo.CurrentCulture)!,
+                AutoUpdater.AppTitle, _args.CurrentVersion, _args.InstalledVersion);
 
-		_args = args;
-		InitializeComponent();
+        if (AutoUpdater.Mandatory && AutoUpdater.UpdateMode == Mode.Forced)
+        {
+            ControlBox = false;
+        }
+    }
 
-		_materialSkinManager = MaterialSkinManager.Instance;
-		_materialSkinManager.EnforceBackcolorOnAllComponents = true;
-		_materialSkinManager.AddFormToManage(this);
-		_materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
-		_materialSkinManager.ColorScheme = new MaterialColorScheme(
-			MaterialPrimary.Indigo500,
-			MaterialPrimary.Indigo700,
-			MaterialPrimary.Indigo100,
-			MaterialAccent.Pink200,
-			MaterialTextShade.WHITE
-		);
+    private async void InitializeBrowserControl()
+    {
+        if (string.IsNullOrEmpty(_args.ChangelogURL))
+        {
+            int reduceHeight = labelReleaseNotes.Height + webBrowser.Height;
+            labelReleaseNotes.Hide();
+            webBrowser.Hide();
+            webView2.Hide();
+            Height -= reduceHeight;
+        }
+        else
+        {
+            var webView2RuntimeFound = false;
+            try
+            {
+                string availableBrowserVersion = CoreWebView2Environment.GetAvailableBrowserVersionString();
+                var requiredMinBrowserVersion = "86.0.616.0";
+                if (!string.IsNullOrEmpty(availableBrowserVersion)
+                    && CoreWebView2Environment.CompareBrowserVersions(availableBrowserVersion,
+                        requiredMinBrowserVersion) >= 0)
+                {
+                    webView2RuntimeFound = true;
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
 
-		InitializeBrowserControl();
-		buttonSkip.Visible = AutoUpdater.ShowSkipButton;
-		buttonRemindLater.Visible = AutoUpdater.ShowRemindLaterButton;
-		var resources = new ComponentResourceManager(typeof(UpdateForm));
-		Text = string.Format(resources.GetString("$this.Text", CultureInfo.CurrentCulture)!,
-			AutoUpdater.AppTitle, _args.CurrentVersion);
-		labelUpdate.Text = string.Format(resources.GetString("labelUpdate.Text", CultureInfo.CurrentCulture)!,
-			AutoUpdater.AppTitle);
-		labelDescription.Text =
-			string.Format(resources.GetString("labelDescription.Text", CultureInfo.CurrentCulture)!,
-				AutoUpdater.AppTitle, _args.CurrentVersion, _args.InstalledVersion);
+            if (webView2RuntimeFound)
+            {
+                webBrowser.Hide();
+                webView2.CoreWebView2InitializationCompleted += WebView_CoreWebView2InitializationCompleted;
+                await webView2.EnsureCoreWebView2Async(
+                    await CoreWebView2Environment.CreateAsync(null, Path.GetTempPath()));
+            }
+            else
+            {
+                UseLatestIE();
+                if (null != AutoUpdater.BasicAuthChangeLog)
+                {
+                    webBrowser.Navigate(_args.ChangelogURL, "", null,
+                        $"Authorization: {AutoUpdater.BasicAuthChangeLog}");
+                }
+                else
+                {
+                    webBrowser.Navigate(_args.ChangelogURL);
+                }
+            }
+        }
+    }
 
-		if (AutoUpdater.Mandatory && AutoUpdater.UpdateMode == Mode.Forced)
-		{
-			ControlBox = false;
-		}
-	}
+    private void WebView_CoreWebView2InitializationCompleted(object sender,
+        CoreWebView2InitializationCompletedEventArgs e)
+    {
+        if (!e.IsSuccess)
+        {
+            if (AutoUpdater.ReportErrors)
+            {
+                MessageBox.Show(this, e.InitializationException.Message, e.InitializationException.GetType().ToString(),
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
 
-	private async void InitializeBrowserControl()
-	{
-		if (string.IsNullOrEmpty(_args.ChangelogURL))
-		{
-			int reduceHeight = labelReleaseNotes.Height + webBrowser.Height;
-			labelReleaseNotes.Hide();
-			webBrowser.Hide();
-			webView2.Hide();
-			Height -= reduceHeight;
-		}
-		else
-		{
-			var webView2RuntimeFound = false;
-			try
-			{
-				string availableBrowserVersion = CoreWebView2Environment.GetAvailableBrowserVersionString();
-				var requiredMinBrowserVersion = "86.0.616.0";
-				if (!string.IsNullOrEmpty(availableBrowserVersion)
-					&& CoreWebView2Environment.CompareBrowserVersions(availableBrowserVersion,
-						requiredMinBrowserVersion) >= 0)
-				{
-					webView2RuntimeFound = true;
-				}
-			}
-			catch (Exception)
-			{
-				// ignored
-			}
+            return;
+        }
 
-			if (webView2RuntimeFound)
-			{
-				webBrowser.Hide();
-				webView2.CoreWebView2InitializationCompleted += WebView_CoreWebView2InitializationCompleted;
-				await webView2.EnsureCoreWebView2Async(
-					await CoreWebView2Environment.CreateAsync(null, Path.GetTempPath()));
-			}
-			else
-			{
-				UseLatestIE();
-				if (null != AutoUpdater.BasicAuthChangeLog)
-				{
-					webBrowser.Navigate(_args.ChangelogURL, "", null,
-						$"Authorization: {AutoUpdater.BasicAuthChangeLog}");
-				}
-				else
-				{
-					webBrowser.Navigate(_args.ChangelogURL);
-				}
-			}
-		}
-	}
+        webView2.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+        webView2.CoreWebView2.Settings.IsStatusBarEnabled = false;
+        webView2.CoreWebView2.Settings.AreDevToolsEnabled = Debugger.IsAttached;
+        webView2.CoreWebView2.Settings.UserAgent = AutoUpdater.GetUserAgent();
+        webView2.CoreWebView2.Profile.ClearBrowsingDataAsync();
+        webView2.Show();
+        webView2.BringToFront();
+        if (null != AutoUpdater.BasicAuthChangeLog)
+        {
+            webView2.CoreWebView2.BasicAuthenticationRequested += delegate(
+                object _,
+                CoreWebView2BasicAuthenticationRequestedEventArgs args)
+            {
+                args.Response.UserName = ((BasicAuthentication)AutoUpdater.BasicAuthChangeLog).Username;
+                args.Response.Password = ((BasicAuthentication)AutoUpdater.BasicAuthChangeLog).Password;
+            };
+        }
 
-	private void WebView_CoreWebView2InitializationCompleted(object sender,
-		CoreWebView2InitializationCompletedEventArgs e)
-	{
-		if (!e.IsSuccess)
-		{
-			if (AutoUpdater.ReportErrors)
-			{
-				MessageBox.Show(this, e.InitializationException.Message, e.InitializationException.GetType().ToString(),
-					MessageBoxButtons.OK, MessageBoxIcon.Error);
-			}
+        webView2.CoreWebView2.Navigate(_args.ChangelogURL);
+    }
 
-			return;
-		}
+    private void UseLatestIE()
+    {
+        int ieValue = webBrowser.Version.Major switch
+        {
+            11 => 11001,
+            10 => 10001,
+            9 => 9999,
+            8 => 8888,
+            7 => 7000,
+            _ => 0
+        };
 
-		webView2.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
-		webView2.CoreWebView2.Settings.IsStatusBarEnabled = false;
-		webView2.CoreWebView2.Settings.AreDevToolsEnabled = Debugger.IsAttached;
-		webView2.CoreWebView2.Settings.UserAgent = AutoUpdater.GetUserAgent();
-		webView2.CoreWebView2.Profile.ClearBrowsingDataAsync();
-		webView2.Show();
-		webView2.BringToFront();
-		if (null != AutoUpdater.BasicAuthChangeLog)
-		{
-			webView2.CoreWebView2.BasicAuthenticationRequested += delegate (
-				object _,
-				CoreWebView2BasicAuthenticationRequestedEventArgs args)
-			{
-				args.Response.UserName = ((BasicAuthentication)AutoUpdater.BasicAuthChangeLog).Username;
-				args.Response.Password = ((BasicAuthentication)AutoUpdater.BasicAuthChangeLog).Password;
-			};
-		}
+        if (ieValue == 0)
+        {
+            return;
+        }
 
-		webView2.CoreWebView2.Navigate(_args.ChangelogURL);
-	}
+        try
+        {
+            using RegistryKey registryKey =
+                Registry.CurrentUser.OpenSubKey(
+                    @"SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION",
+                    true);
+            registryKey?.SetValue(
+                Path.GetFileName(Process.GetCurrentProcess().MainModule?.FileName ??
+                                 GetEntryAssembly()?.Location ?? Application.ExecutablePath),
+                ieValue,
+                RegistryValueKind.DWord);
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+    }
 
-	private void UseLatestIE()
-	{
-		int ieValue = webBrowser.Version.Major switch
-		{
-			11 => 11001,
-			10 => 10001,
-			9 => 9999,
-			8 => 8888,
-			7 => 7000,
-			_ => 0
-		};
+    private void UpdateFormLoad(object sender, EventArgs e)
+    {
+        var labelSize = new Size(webBrowser.Width, 0);
+        labelDescription.MaximumSize = labelUpdate.MaximumSize = labelSize;
+    }
 
-		if (ieValue == 0)
-		{
-			return;
-		}
+    private void ButtonUpdateClick(object sender, EventArgs e)
+    {
+        if (AutoUpdater.OpenDownloadPage)
+        {
+            var processStartInfo = new ProcessStartInfo(_args.DownloadURL);
 
-		try
-		{
-			using RegistryKey registryKey =
-				Registry.CurrentUser.OpenSubKey(
-					@"SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION",
-					true);
-			registryKey?.SetValue(
-				Path.GetFileName(Process.GetCurrentProcess().MainModule?.FileName ??
-								 GetEntryAssembly()?.Location ?? Application.ExecutablePath),
-				ieValue,
-				RegistryValueKind.DWord);
-		}
-		catch (Exception)
-		{
-			// ignored
-		}
-	}
+            Process.Start(processStartInfo);
 
-	private void UpdateFormLoad(object sender, EventArgs e)
-	{
-		var labelSize = new Size(webBrowser.Width, 0);
-		labelDescription.MaximumSize = labelUpdate.MaximumSize = labelSize;
-	}
+            DialogResult = DialogResult.OK;
+        }
+        else
+        {
+            if (AutoUpdater.DownloadUpdate(_args))
+            {
+                DialogResult = DialogResult.OK;
+            }
+        }
+    }
 
-	private void ButtonUpdateClick(object sender, EventArgs e)
-	{
-		if (AutoUpdater.OpenDownloadPage)
-		{
-			var processStartInfo = new ProcessStartInfo(_args.DownloadURL);
+    private void ButtonSkipClick(object sender, EventArgs e)
+    {
+        AutoUpdater.PersistenceProvider.SetSkippedVersion(new Version(_args.CurrentVersion));
+    }
 
-			Process.Start(processStartInfo);
+    private void ButtonRemindLaterClick(object sender, EventArgs e)
+    {
+        if (AutoUpdater.LetUserSelectRemindLater)
+        {
+            using var remindLaterForm = new RemindLaterForm();
+            DialogResult dialogResult = remindLaterForm.ShowDialog(this);
 
-			DialogResult = DialogResult.OK;
-		}
-		else
-		{
-			if (AutoUpdater.DownloadUpdate(_args))
-			{
-				DialogResult = DialogResult.OK;
-			}
-		}
-	}
+            switch (dialogResult)
+            {
+                case DialogResult.OK:
+                    AutoUpdater.RemindLaterTimeSpan = remindLaterForm.RemindLaterFormat;
+                    AutoUpdater.RemindLaterAt = remindLaterForm.RemindLaterAt;
+                    break;
+                case DialogResult.Abort:
+                    ButtonUpdateClick(sender, e);
+                    return;
+                default:
+                    return;
+            }
+        }
 
-	private void ButtonSkipClick(object sender, EventArgs e)
-	{
-		AutoUpdater.PersistenceProvider.SetSkippedVersion(new Version(_args.CurrentVersion));
-	}
+        AutoUpdater.PersistenceProvider.SetSkippedVersion(null);
 
-	private void ButtonRemindLaterClick(object sender, EventArgs e)
-	{
-		if (AutoUpdater.LetUserSelectRemindLater)
-		{
-			using var remindLaterForm = new RemindLaterForm();
-			DialogResult dialogResult = remindLaterForm.ShowDialog(this);
+        DateTime remindLaterDateTime = AutoUpdater.RemindLaterTimeSpan switch
+        {
+            RemindLaterFormat.Days => DateTime.Now + TimeSpan.FromDays(AutoUpdater.RemindLaterAt),
+            RemindLaterFormat.Hours => DateTime.Now + TimeSpan.FromHours(AutoUpdater.RemindLaterAt),
+            RemindLaterFormat.Minutes => DateTime.Now + TimeSpan.FromMinutes(AutoUpdater.RemindLaterAt),
+            _ => DateTime.Now
+        };
 
-			switch (dialogResult)
-			{
-				case DialogResult.OK:
-					AutoUpdater.RemindLaterTimeSpan = remindLaterForm.RemindLaterFormat;
-					AutoUpdater.RemindLaterAt = remindLaterForm.RemindLaterAt;
-					break;
-				case DialogResult.Abort:
-					ButtonUpdateClick(sender, e);
-					return;
-				default:
-					return;
-			}
-		}
+        AutoUpdater.PersistenceProvider.SetRemindLater(remindLaterDateTime);
+        AutoUpdater.SetTimer(remindLaterDateTime);
 
-		AutoUpdater.PersistenceProvider.SetSkippedVersion(null);
+        DialogResult = DialogResult.Cancel;
+    }
 
-		DateTime remindLaterDateTime = AutoUpdater.RemindLaterTimeSpan switch
-		{
-			RemindLaterFormat.Days => DateTime.Now + TimeSpan.FromDays(AutoUpdater.RemindLaterAt),
-			RemindLaterFormat.Hours => DateTime.Now + TimeSpan.FromHours(AutoUpdater.RemindLaterAt),
-			RemindLaterFormat.Minutes => DateTime.Now + TimeSpan.FromMinutes(AutoUpdater.RemindLaterAt),
-			_ => DateTime.Now
-		};
+    private void UpdateForm_FormClosed(object sender, FormClosedEventArgs e)
+    {
+        AutoUpdater.Running = false;
+    }
 
-		AutoUpdater.PersistenceProvider.SetRemindLater(remindLaterDateTime);
-		AutoUpdater.SetTimer(remindLaterDateTime);
-
-		DialogResult = DialogResult.Cancel;
-	}
-
-	private void UpdateForm_FormClosed(object sender, FormClosedEventArgs e)
-	{
-		AutoUpdater.Running = false;
-	}
-
-	private void UpdateForm_FormClosing(object sender, FormClosingEventArgs e)
-	{
-		if (AutoUpdater.Mandatory && AutoUpdater.UpdateMode == Mode.Forced)
-		{
-			AutoUpdater.Exit();
-		}
-	}
+    private void UpdateForm_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        if (AutoUpdater.Mandatory && AutoUpdater.UpdateMode == Mode.Forced)
+        {
+            AutoUpdater.Exit();
+        }
+    }
 }
